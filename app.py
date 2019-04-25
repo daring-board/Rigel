4715b0638a4d5670ad64d7b0397d86086b15ea44
daring-board <mahotox101500@gmail.com>
2019-04-25 21:45:37 +0900

    port番号変更

--- app.py
+++ app.py
@@ -120,4 +120,4 @@ def abortWithServerError(reason, debug={}):
 
 if __name__ == "__main__":
     print(" * Flask starting server...")
-    app.run(host='0.0.0.0',port=5000)
+    app.run(host='0.0.0.0',port=5001)
