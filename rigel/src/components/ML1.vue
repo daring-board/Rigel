478c36684cd012389d42fc178119b0ee7135da1d
Mahoto Iwasa <mahotox101500@gmail.com>
2020-02-15 22:28:25 +0900

    学習器のモデルダウンロードを任意にできるように修正

--- rigel/src/components/ML1.vue
+++ rigel/src/components/ML1.vue
@@ -1,16 +1,22 @@
 <template>
   <div class="ml1">
-    <video autoplay playsinline muted id="webcam" width="224" height="224"></video>
-    <div>{{message}}</div>
-
-    <div>
-      <p>下のボタンを押下するとAIが画像を覚えてくれます。</p>
-      <p>『Aの画像はこれだよ』という気持ちでボタンをクリックしてください。</p>
-      <p>AIは画像を3種類だけ覚える事ができます。（A, B, Cの3種類）</p>
+    <video autoplay playsinline muted id="webcam" :width="width" :height="height"></video>
+    <div v-if="!is_try">
+      <b-button @click="start_process">賢くなるAIを試す</b-button>
     </div>
-    <b-button class='button' @click="addExample(0)">Add A</b-button>
-    <b-button class='button' @click="addExample(1)">Add B</b-button>
-    <b-button class='button' @click="addExample(2)">Add C</b-button>
+    <div v-else>
+      <b-spinner v-if="is_loading" variant="primary" label="Spinning"></b-spinner>
+      <div>{{message}}</div>
+
+      <div>
+        <p>下のボタンを押下するとAIが画像を覚えてくれます。</p>
+        <p>『Aの画像はこれだよ』という気持ちでボタンをクリックしてください。</p>
+        <p>AIは画像を3種類だけ覚える事ができます。（A, B, Cの3種類）</p>
+      </div>
+      <b-button class='button' @click="addExample(0)">Add A</b-button>
+      <b-button class='button' @click="addExample(1)">Add B</b-button>
+      <b-button class='button' @click="addExample(2)">Add C</b-button>
+    </div>
   </div>
 </template>
 
@@ -23,48 +29,14 @@ export default {
   name: 'ML1',
   data: function(){
     return {
+      is_loading: false,
+      is_try: false,
       net: null,
       classifier: null,
       message: '',
       webcam: null,
-    }
-  },
-  mounted: async function(){
-    /* eslint-disable */
-    const webcamEl = document.getElementById('webcam')
-    console.log('Loading mobilenet..')
-    this.net = await mobilenet.load()
-    console.log('Successfully loaded model')
-
-    console.log('Create Classifier')
-    this.classifier = knn.create()
-    console.log('Successfully created classifier')
-
-    this.webcam = await tf.data.webcam(webcamEl)
-    
-    while (true) {
-      if (this.classifier.getNumClasses() > 0) {
-        const img = await this.webcam.capture()
-        const activation = this.net.infer(img, 'conv_preds')
-        console.log(activation)
-        const result = await this.classifier.predictClass(activation);
-        const classes = ['A', 'B', 'C']
-        var pre = ''
-        var post = ''
-        if(result.confidences[result.label] > 0.9){
-          pre = ''
-        }else if(result.confidences[result.label] > 0.6){
-          pre = 'たぶん、'
-        }else if(result.confidences[result.label] > 0.3){
-          pre = 'もしかして、'
-          post = '?'
-        }else{
-          continue
-        }
-        this.message = `${pre}${classes[result.label]}が見えている${post}\n`
-        img.dispose();
-      }
-      await tf.nextFrame();
+      width: 0,
+      height: 0
     }
   },
   methods: {
@@ -73,6 +45,53 @@ export default {
       const activation = this.net.infer(img, 'conv_preds')
       this.classifier.addExample(activation, classId)
       img.dispose()
+    },
+    start_process: function(){
+      this.is_try = true
+      this.processing()
+    },
+    processing: async function(){
+      /* eslint-disable */
+      this.is_loading = true
+
+      console.log('Loading mobilenet..')
+      this.net = await mobilenet.load()
+      console.log('Successfully loaded model')
+
+      console.log('Create Classifier')
+      this.classifier = knn.create()
+      console.log('Successfully created classifier')
+      this.is_loading = false
+
+      this.width = 224
+      this.height = 224
+      const webcamEl = document.getElementById('webcam')
+      this.webcam = await tf.data.webcam(webcamEl)
+      
+      while (true) {
+        if (this.classifier.getNumClasses() > 0) {
+          const img = await this.webcam.capture()
+          const activation = this.net.infer(img, 'conv_preds')
+          console.log(activation)
+          const result = await this.classifier.predictClass(activation);
+          const classes = ['A', 'B', 'C']
+          var pre = ''
+          var post = ''
+          if(result.confidences[result.label] > 0.9){
+            pre = ''
+          }else if(result.confidences[result.label] > 0.6){
+            pre = 'たぶん、'
+          }else if(result.confidences[result.label] > 0.3){
+            pre = 'もしかして、'
+            post = '?'
+          }else{
+            continue
+          }
+          this.message = `${pre}${classes[result.label]}が見えている${post}\n`
+          img.dispose();
+        }
+        await tf.nextFrame();
+      }
     }
   }
 }
